a4ebe77df64e8c89df90b9ad8e4a2ec110f73de4
todda5181 <todda5181@gmail.com>
2026-05-04 13:30:16 -0400

    Update private-client-intake.js

--- pages/private-client-intake.js
+++ pages/private-client-intake.js
@@ -9,7 +9,7 @@ export default function PrivateClientIntake() {
 
     const formData = new FormData(e.target);
 
-    const response = await fetch("https://formspree.io/f/mzdykgry", {
+    const response = await https://formspree.io/f/xojreojj, {
       method: "POST",
       body: formData,
       headers: { Accept: "application/json" },
